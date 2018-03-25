cef18517fa32c982a452fa6b9309967670f903d9
Rudi Niemeijer <rudiniemeijer@gmail.com>
2018-03-25 21:18:46 +0200

    Update python_org_search.py

--- python_org_search.py
+++ python_org_search.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-driver = webdriver.Chrome('/Users/rudi/Dropbox/KPN/Selenium/chromedriver')
+driver = webdriver.Chrome('enter the location of the Chromedriver here')
 driver.get("http://www.python.org") 
 assert "Python" in driver.title
 elem = driver.find_element_by_name("q")
