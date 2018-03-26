8520274cb48f9ee0445803d54b5e30a2e6b9db7b
Rudi Niemeijer <rudiniemeijer@gmail.com>
2018-03-26 19:55:36 +0200

    Update testFrameWork.py

--- testFrameWork.py
+++ testFrameWork.py
@@ -9,7 +9,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import TimeoutException
 
-
 # Stuff that needs to go in the configuration
 settings = dict(
     driverpath = './chromedriver',
