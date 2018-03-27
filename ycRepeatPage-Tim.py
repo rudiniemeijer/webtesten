c4331e2ec9a57c06ed26103b29ce674df7286215
Rudi Niemeijer <rudiniemeijer@gmail.com>
2018-03-27 10:44:45 +0200

    Create ycRepeatPage-Tim.py

---
+++ ycRepeatPage-Tim.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+driver = webdriver.Chrome('./chromedriver')
+
+for i in range(25):
+    driver.get("https://www.youngcapital.nl/")
+    assert "YoungCapital" in driver.title
+
+    elem = driver.find_element_by_xpath('//*[@placeholder="Plaatsnaam of postcode"]')
+    elem.send_keys("Zwolle")
+    knop = driver.find_element_by_xpath('//*[@id="job_opening_search"]/div/div[4]/div[1]/button')
+    knop.click()
+
+driver.close()
