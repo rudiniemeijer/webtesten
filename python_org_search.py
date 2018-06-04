6acfd31d49854a3025a73e482f24e97e85b81e52
Rudi Niemeijer <rudiniemeijer@gmail.com>
2018-06-04 15:15:46 +0200

    Update python_org_search.py

--- python_org_search.py
+++ python_org_search.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-driver = webdriver.Chrome('enter the location of the Chromedriver here')
+driver = webdriver.Chrome('enter the location of the Chromedriver here') # like c:\\ .. \\chromedriver.exe
 driver.get("http://www.google.nl") 
 assert "Google" in driver.title
 elem = driver.find_element_by_name("q")
